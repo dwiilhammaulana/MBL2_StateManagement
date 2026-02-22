0c3b1bc9374d37ec119611cf837927ec191cb8e5
dwiilhammaulana <1123150008@global.ac.id>
2026-02-22 16:44:02 +0700

    commit 8 soal quis

--- lib/counter_model.dart
+++ lib/counter_model.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class CounterModel{
+class CounterModel extends ChangeNotifier {
   int _angka = 0;
 
   int get angka => _angka;
 
   void increment() {
     _angka++;
+    notifyListeners();
   }
 }
